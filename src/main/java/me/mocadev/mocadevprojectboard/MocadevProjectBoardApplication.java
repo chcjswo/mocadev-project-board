f5cfe87b632898f24d51c8d490e32545db170ee7
tony <chcjswo@gmail.com>
2022-09-26 00:34:57 +0900

    #2 스프링 부트 시작
    * 기본 디펜던시로 시작

---
+++ src/main/java/me/mocadev/mocadevprojectboard/MocadevProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package me.mocadev.mocadevprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocadevProjectBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocadevProjectBoardApplication.class, args);
+	}
+
+}

32f9f2677dd3391707f63f028843bf606b2d514e
tony <chcjswo@gmail.com>
2022-11-04 01:08:54 +0900

    #1 - Spring Data Rest + Hal Explore 적용

--- src/main/java/me/mocadev/mocadevprojectboard/repository/ArticleCommentRepository.java
+++ src/main/java/me/mocadev/mocadevprojectboard/repository/ArticleCommentRepository.java
@@ -2,7 +2,9 @@ package me.mocadev.mocadevprojectboard.repository;
 
 import me.mocadev.mocadevprojectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
 }
